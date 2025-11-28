445d92b94b58fc9e18b8332e391b40899bca3d64
Derrick <120696745+drtinaz@users.noreply.github.com>
2025-11-28 14:36:11 -0700

    Increase sleep duration before restarting driver

--- download.sh
+++ download.sh
@@ -187,7 +187,7 @@ if [ ! -f ${driver_path}/${driver_name}/config.ini ]; then
     echo ""
  else
     echo "Restarting driver to apply new version..."
-    sleep 1
+    sleep 3
     /bin/bash ${driver_path}/${driver_name}/restart.sh
 fi
 
